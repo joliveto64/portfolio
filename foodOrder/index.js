d61e15e5c337374391bc4fa1acc20824ecd32691
joliveto64 <joliveto64@gmail.com>
2023-06-06 02:05:28 -0400

    done with this for now

--- foodOrder/index.js
+++ foodOrder/index.js
@@ -3,12 +3,9 @@ const main = document.querySelector(".main");
 const orderContainer = document.querySelector(".order-container");
 const bottomContainer = document.querySelector(".bottom-container");
 const modal = document.querySelector(".modal");
-const payBtn = document.querySelector(".pay-button");
 const thanks = document.querySelector(".thanks-message");
-const nameInput = document.querySelector("name");
 const form = document.querySelector("form");
 
-// let totalPrice = 0;
 let orderArray = [];
 
 renderItemsHtml();
@@ -35,26 +32,13 @@ function renderOrdersHtml() {
   orderContainer.innerHTML = "";
   orderArray.forEach((item) => {
     orderContainer.innerHTML += `<div class="added-item">
-      <div class="added-item-name">${item.name}<button class="remove-btn" data-id="${item.id}">remove</button></div>
+      <div class="added-item-name">${
+        item.name
+      }<button class="remove-btn" data-id="${item.id}">remove</button></div>
       <div class="added-item-price">${"$" + item.price}</div>
     </div>`;
   });
 }
-
-// Neater way to handle clicks:
-// document.addEventListener("click", (event) => {
-//   // These just handle changing your internal state (ie orderArray and menuArray), no ui work
-//   if (event === 'add') {
-//     handleAddClick(event);
-//   } else if (event === 'remove') {
-//     handleRmClick(event);
-//   } else if (event === 'order') {
-//     handleOrderClick(event);
-//   }
-
-//   // redraw from scratch all the parts of your UI that maybe change 
-//   // here using your internal state (ie orderArray and menuArray)
-// });
 
 document.addEventListener("click", (event) => {
   // HACK!
@@ -66,15 +50,10 @@ document.addEventListener("click", (event) => {
   orderContainer.classList.remove("hidden");
   bottomContainer.classList.remove("hide-bottom");
 
-  // ADD ITEMS TO THE ORDER SECTION**************
+  // ADD ITEMS TO THE ORDER ARRAY**************
   if (event.target.classList.contains("add-btn")) {
     menuArray.forEach((item) => {
       if (event.target.dataset.id === item.id.toString()) {
-      //   orderContainer.innerHTML += `<div class="added-item">
-      //   <div class="added-item-name">${item.name}<button class="remove-btn" data-id="${item.id}">remove</button></div>
-      //   <div class="added-item-price">${"$" + item.price}</div>
-      // </div>`;
-        // totalPrice += item.price;
         orderArray.push(item);
         renderOrdersHtml();
       }
@@ -85,7 +64,6 @@ document.addEventListener("click", (event) => {
     for (let i = 0; i < orderArray.length; i++) {
       const item = orderArray[i];
       if (item.id === parseInt(event.target.dataset.id)) {
-        // totalPrice -= item.price;
         orderArray.splice(i, 1);
         renderOrdersHtml();
         break;
@@ -93,26 +71,18 @@ document.addEventListener("click", (event) => {
     }
   }
 
-  // updatePrice();
-  renderTotalSection();
+  renderBottomSection();
 
-  const orderBtn = document.querySelector(".order-btn");
   const clickedInsideModal = event.target.closest(".modal");
 
-  if (event.target === orderBtn) {
+  if (event.target.classList.contains("order-btn")) {
     modal.classList.remove("hide-modal");
   } else if (!clickedInsideModal) {
     modal.classList.add("hide-modal");
   }
-
-  // const removeBtn = document.querySelector("remove-btn");
-
-  // // if (event.target === removeBtn) {
-  // //   console.log(event.target.dataset.id);
-  // // }
 });
 
-function renderTotalSection() {
+function renderBottomSection() {
   let totalPrice = 0;
   orderArray.forEach((item) => {
     totalPrice += item.price;
@@ -127,23 +97,11 @@ function renderTotalSection() {
   </div>`;
 }
 
-// // function updatePrice() {
-// //   let totalPrice = 0;
-// //   orderArray.forEach((item) => {
-// //     totalPrice += item.price;
-// //   });
-
-// //   const totalPriceDiv = document.querySelector(".total-price");
-// //   totalPriceDiv.innerText = "$" + totalPrice;
-// }
-
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   modal.classList.add("hide-modal");
   orderContainer.classList.add("hidden");
   bottomContainer.classList.add("hide-bottom");
   thanks.classList.remove("hide-thanks");
+  orderArray = [];
 });
-
-// 1. clicked anywhere works to show bottom content, when I tried to fix it it stopped displying added items
-// 2. can't figure out how to make the remove button work
