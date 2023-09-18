cbfd61e06227239b5b613b8604ff135364a1f3e6
joliveto64 <joliveto64@gmail.com>
2023-09-18 21:28:06 +0100

    added typing animation

--- public/index.js
+++ public/index.js
@@ -1,70 +1,20 @@
-// SEARCH STUFF ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+const text2 =
+  "I'm John! I have experience with HTML, CCS, JavaScript and React.";
+const text3 = "(I also play the drums and lots of Monster Hunter)";
 
-// create variables
-const searchButton = document.getElementById("search-button");
-const searchResultsContainer = document.getElementById("search-results-string");
-const htmlTags = document.getElementsByTagName("a");
-const linksArr = [];
+const el2 = document.querySelector(".line2");
+const el3 = document.querySelector(".line3");
 
-// populate links array
-for (let i = 0; i < htmlTags.length; i++) {
-  linksArr.push(htmlTags[i].href);
+function typeText(str, element) {
+  for (let i = 0; i < str.length; i++) {
+    setTimeout(() => {
+      element.innerHTML += str[i];
+    }, 45 * i);
+  }
 }
 
-// event listener for click
-searchButton.addEventListener("click", () => {
-  let filteredArr = [];
-  const resultsArr = [];
-  const inputArr = document
-    .getElementById("search-input")
-    .value.toLowerCase()
-    .trim()
-    .split(" ");
-
-  // filter inputArr to get rid of empty spaces
-  for (let index = 0; index < inputArr.length; index++) {
-    if (inputArr[index] != "") {
-      filteredArr.push(inputArr[index]);
-    }
-  }
-
-  // nested loop to compare the user input array to the urls array
-  for (let i = 0; i < filteredArr.length; i++) {
-    for (let j = 0; j < linksArr.length; j++) {
-      if (linksArr[j].includes(filteredArr[i])) {
-        resultsArr.push(linksArr[j]);
-      } else {
-        continue;
-      }
-    }
-  }
-
-  if (resultsArr.length === 0) {
-    resultsArr.push("No match");
-  }
-
-  searchResultsContainer.innerHTML = resultsArr;
-  searchResultsContainer.classList.remove("hidden");
-
-  searchResultsContainer.addEventListener("click", () => {
-    for (let index = 0; index < resultsArr.length; index++) {
-      window.location.href = resultsArr[index];
-    }
-  });
-});
-
-// events to close the search popup
-document.addEventListener("keydown", function (event) {
-  if (event.key === "Escape") {
-    searchResultsContainer.classList.add("hidden");
-  }
-});
-
-document.addEventListener("click", (event) => {
-  if (
-    !searchResultsContainer.contains(event.target) &&
-    !searchButton.contains(event.target)
-  ) {
-    searchResultsContainer.classList.add("hidden");
-  }
-});
+typeText(text2, el2);
+setTimeout(() => {
+  console.log("now");
+  typeText(text3, el3);
+}, 50 * text2.length);
